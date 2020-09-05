338bd0714a9e06f8c05ae58a86c6f6409dd5edc8
Lesair Valmont <ixra.net@gmail.com>
2020-09-05 14:47:08 -0500

    HasRepeatedCharacters extension method.

---
+++ src/CSharp/Extensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ardalis.GuardClauses;
+
+namespace CSharp
+{
+    /// <summary>
+    ///     Common useful extensions.
+    /// </summary>
+    public static class Extensions
+    {
+        public static IEnumerable<Func<string, bool>> HasRepeatedCharactersImplementations
+        {
+            get
+            {
+                return new Func<string, bool>[]
+                {
+                    HasRepeatedCharactersLinqImplementation,
+                    HasRepeatedCharactersDictionaryImplementation
+                };
+            }
+        }
+
+        /// <summary>
+        ///     Determines if a string contains repeated characters.
+        /// </summary>
+        /// <param name="s">The string to evaluate.</param>
+        /// <param name="implementation">Algorithm implementation to use.</param>
+        /// <returns>True if it contains repeated characters, false otherwise.</returns>
+        public static bool HasRepeatedCharacters(this string s, Func<string, bool> implementation = null)
+        {
+            if (implementation == null)
+                implementation = HasRepeatedCharactersDictionaryImplementation;
+
+            return implementation(s);
+        }
+
+        /// <summary>
+        ///     <see cref="HasRepeatedCharacters" /> iterative implementation with Linq.
+        ///     Time complexity: O(n).
+        ///     Space complexity: O(n).
+        /// </summary>
+        private static bool HasRepeatedCharactersLinqImplementation(this string s)
+        {
+            return s
+                .Select(c => c)
+                .GroupBy(c => c)
+                .Select(g => g.Count())
+                .Any(c => c > 1);
+        }
+
+        /// <summary>
+        ///     <see cref="HasRepeatedCharacters" /> greedy iterative implementation with a Dictionary.
+        ///     Time complexity: O(n).
+        ///     Space complexity: O(n).
+        /// </summary>
+        private static bool HasRepeatedCharactersDictionaryImplementation(this string s)
+        {
+            Guard.Against.Null(s, nameof(s));
+
+            var dictionary = new Dictionary<char, int>();
+            foreach (var c in s)
+            {
+                if (dictionary.ContainsKey(c))
+                    return true;
+                dictionary.Add(c, 1);
+            }
+
+            return false;
+        }
+    }
+}
