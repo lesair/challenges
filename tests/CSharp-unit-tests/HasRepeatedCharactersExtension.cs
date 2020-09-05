48567e339796304ec0cfe0168e3d94ec261d6357
Lesair Valmont <ixra.net@gmail.com>
2020-09-05 14:47:48 -0500

    HasRepeatedCharacters extension method unit tests.

---
+++ tests/CSharp-unit-tests/HasRepeatedCharactersExtension.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class HasRepeatedCharactersExtension
+    {
+        private static void TestImplementations(string s, bool expected)
+        {
+            foreach (var implementation in Extensions.HasRepeatedCharactersImplementations)
+                s.HasRepeatedCharacters(implementation).ShouldBe(expected);
+        }
+
+        private static void TestImplementationsThrow<T>(string s) where T : Exception
+        {
+            foreach (var implementation in Extensions.HasRepeatedCharactersImplementations)
+                Assert.Throws<T>(() => s.HasRepeatedCharacters(implementation));
+        }
+
+        [Fact]
+        public void ReturnsFalseBecauseIsNotCaseSensitive()
+        {
+            const bool expected = false;
+            TestImplementations("abcdefghiIjkmnlopqrstuvwxyz", expected);
+        }
+
+        [Fact]
+        public void ReturnsFalseWithEmptyStrings()
+        {
+            const bool expected = false;
+            TestImplementations("", expected);
+        }
+
+        [Fact]
+        public void ReturnsFalseWithoutDuplicatedCharacters()
+        {
+            const bool expected = false;
+            TestImplementations("abcdefghijkmnlopqrstuvwxyz", expected);
+        }
+
+        [Fact]
+        public void ReturnsTrueWithDuplicatedCharacters()
+        {
+            const bool expected = true;
+            TestImplementations("abcdefghijkmnlopqrstuvwxybz", expected);
+        }
+
+        [Fact]
+        public void ThrowsArgumentNullExceptionWhenStringIsNull()
+        {
+            TestImplementationsThrow<ArgumentNullException>(null);
+        }
+    }
+}
