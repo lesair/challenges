b536858ddc4699178edc7da3ae3fd421b240186e
Lesair Valmont <ixra.net@gmail.com>
2020-09-11 17:20:45 -0500

    Added new int extension.
    For calculating an int division rounded up to the quotient's ceiliing.

---
+++ src/CSharp/Library/Extensions/IntExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharp.Library.Extensions
+{
+    public static class IntExtensions
+    {
+        /// <summary>
+        ///     Does an integer division rounding the result up to the ceiling. The native integer division rounds down to the
+        ///     floor.
+        ///     https://stackoverflow.com/a/63012251/4376148
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int RoundUpDivideBy(this int a, int b)
+        {
+            var result = Math.DivRem(a, b, out var remainder);
+            if (remainder != 0) result++;
+            return result;
+        }
+    }
+}
