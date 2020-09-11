fd65211159f026ad8ceeeb41fd73997d4ddc6e58
Lesair Valmont <ixra.net@gmail.com>
2020-09-11 17:22:54 -0500

    Added tree data structure-related helper library.

---
+++ src/CSharp/Library/Tree/Node.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp.Library.Tree
+{
+    /// <summary>
+    ///     A generic tree node. A tree has a value and children, and the children are themselves trees; the value and children
+    ///     of the tree are interpreted as the value of the root node and the subtrees of the children of the root node. To
+    ///     allow finite trees, the list of children is allowed to be either empty (in which case trees can be required
+    ///     to be non-empty, an "empty tree" instead being represented by a forest of zero trees), or allow trees to be empty,
+    ///     in which case the list of children can be of fixed size (branching factor, especially 2 or "binary"), if desired.
+    ///     https://en.wikipedia.org/wiki/Tree_(data_structure)
+    /// </summary>
+    /// <typeparam name="T">Node data type.</typeparam>
+    public class Node<T>
+    {
+        public Node()
+        {
+            Children = new List<Node<T>>();
+        }
+
+        public Node(T data)
+        {
+            Data = data;
+            Children = new List<Node<T>>();
+        }
+
+        public Node(T data, IEnumerable<Node<T>> children)
+        {
+            Data = data;
+            Children = children;
+        }
+
+        /// <summary>
+        ///     Node data.
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        ///     Node children.
+        /// </summary>
+        public IEnumerable<Node<T>> Children { get; set; }
+
+        public override string ToString()
+        {
+            var childrenData = Children != null
+                ? $" ({string.Join(", ", Children.Select(child => child == null ? "null" : child.Data.ToString()))})"
+                : string.Empty;
+            return $"{Data}{childrenData}";
+        }
+    }
+}
---
+++ src/CSharp/Library/Tree/TreeManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ardalis.GuardClauses;
+
+namespace CSharp.Library.Tree
+{
+    public class TreeManager<T>
+    {
+        private readonly Dictionary<string, Node<T>> _nodes = new Dictionary<string, Node<T>>();
+
+        public TreeManager(T rootNodeData, string rootNodeName = null)
+        {
+            if (rootNodeName == null)
+                rootNodeName = rootNodeData.ToString();
+            var rootNode = new Node<T>(rootNodeData);
+            _nodes.Add(rootNodeName, rootNode);
+        }
+
+        public Node<T> this[string nodeName] => _nodes[nodeName];
+
+        public void AddChildNodeToParent(string parentNodeName, T nodeData, string nodeName = null)
+        {
+            Guard.Against.Null(parentNodeName, nameof(parentNodeName));
+
+            if (nodeName == null)
+                nodeName = nodeData.ToString();
+            var parentNode = _nodes[parentNodeName];
+            var childNode = new Node<T>(nodeData);
+            _nodes.Add(nodeName, childNode);
+            parentNode.Children = parentNode.Children.Append(childNode);
+        }
+
+        public void AddChildrenNodesToParent(string parentNodeName, IList<(T nodeData, string nodeName)> nodesData)
+        {
+            Guard.Against.Null(parentNodeName, nameof(parentNodeName));
+            Guard.Against.Null(nodesData, nameof(nodesData));
+
+            var parentNode = _nodes[parentNodeName];
+            var newChildNodes = new List<Node<T>>();
+            foreach (var (nodeData, nodeName) in nodesData)
+            {
+                var childNode = new Node<T>(nodeData);
+                _nodes.Add(nodeName, childNode);
+                newChildNodes.Add(childNode);
+            }
+
+            parentNode.Children = parentNode.Children.Concat(newChildNodes);
+        }
+
+        public void AddChildrenNodesToParent(string parentNodeName, IList<T> dataItems)
+        {
+            var nodesData = dataItems.Select(d => (d, d.ToString())).ToList();
+            AddChildrenNodesToParent(parentNodeName, nodesData);
+        }
+    }
+}
