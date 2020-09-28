ab561589002372022c33b0275d0f2cf500010b92
Lesair Valmont <ixra.net@gmail.com>
2020-09-28 17:08:07 -0500

    BaseTest.
    Metclass with common behavior for all tests.

---
+++ tests/CSharp-unit-tests/BaseTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace CSharp
+{
+    public abstract class BaseTest
+    {
+        protected string ImplementationMethodsPrefix = string.Empty;
+        protected string ImplementationMethodsSuffix = "Implementation";
+        protected IReflect TypeToTest;
+
+        protected IEnumerable<MethodInfo> ImplementationsToTest()
+        {
+            var infos = TypeToTest.GetMethods(BindingFlags.Public | BindingFlags.Static);
+            foreach (var methodInfo in infos)
+                if (methodInfo.Name.StartsWith(ImplementationMethodsPrefix) &&
+                    methodInfo.Name.EndsWith(ImplementationMethodsSuffix))
+                    yield return methodInfo;
+        }
+    }
+}
