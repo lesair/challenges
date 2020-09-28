68af94f459e8d603c5f98d8f73628354f622b118
Lesair Valmont <ixra.net@gmail.com>
2020-09-28 17:02:45 -0500

    String extensions refactoring.
    Removed implementation input parameter from entry point and code documenting enhancements.

--- src/CSharp/Library/Extensions/StringExtensions.cs
+++ src/CSharp/Library/Extensions/StringExtensions.cs
@@ -1,39 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Ardalis.GuardClauses;
 
 namespace CSharp.Library.Extensions
 {
     /// <summary>
-    ///     Common useful extensions.
+    ///     Useful string extensions.
     /// </summary>
     public static class StringExtensions
     {
-        public static IEnumerable<Func<string, bool>> HasRepeatedCharactersImplementations
-        {
-            get
-            {
-                return new Func<string, bool>[]
-                {
-                    HasRepeatedCharactersLinqImplementation,
-                    HasRepeatedCharactersDictionaryImplementation
-                };
-            }
-        }
-
         /// <summary>
         ///     Determines if a string contains repeated characters.
         /// </summary>
         /// <param name="s">The string to evaluate.</param>
-        /// <param name="implementation">Algorithm implementation to use.</param>
         /// <returns>True if it contains repeated characters, false otherwise.</returns>
-        public static bool HasRepeatedCharacters(this string s, Func<string, bool> implementation = null)
+        public static bool HasRepeatedCharacters(this string s)
         {
-            if (implementation == null)
-                implementation = HasRepeatedCharactersDictionaryImplementation;
-
-            return implementation(s);
+            return HasRepeatedCharactersDictionaryImplementation(s);
         }
 
         /// <summary>
