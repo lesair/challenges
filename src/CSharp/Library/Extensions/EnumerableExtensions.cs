1e20beb184816fed1b8a85804f2920142f811ffe
Lesair Valmont <ixra.net@gmail.com>
2020-09-28 17:41:11 -0500

    Intersperse Enumerable extension.

---
+++ src/CSharp/Library/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp.Library.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        ///     Intersperses an <see cref="element" /> between the elements of the <see cref="source" /> enumeration. Inspired by
+        ///     Haskell, see: https://hoogle.haskell.org/?hoogle=intersperse
+        /// </summary>
+        /// <typeparam name="T">Enumeration type.</typeparam>
+        /// <param name="source">Enumeration source.</param>
+        /// <param name="element">Separator to intersperse between the elements of the enumeration.</param>
+        /// <param name="options">Specifies the leading and trailing intersperse behavior.</param>
+        /// <returns>The enumeration <see cref="source" /> with the <see cref="element" /> interspersed between its elements.</returns>
+        public static IEnumerable<T> Intersperse<T>(this IEnumerable<T> source, T element, IntersperseOptions options)
+        {
+            if (options == IntersperseOptions.Leading) yield return element;
+
+            var first = true;
+            foreach (var value in source)
+            {
+                if (!first) yield return element;
+                yield return value;
+                first = false;
+            }
+
+            if (options == IntersperseOptions.Trailing) yield return element;
+        }
+    }
+
+    /// <summary>
+    ///     Specifies the leading and trailing intersperse behavior.
+    /// </summary>
+    [Flags]
+    public enum IntersperseOptions
+    {
+        /// <summary>
+        ///     The return enumeration won't contain a leading or trailing element.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        ///     The return enumeration will contain a leading element.
+        /// </summary>
+        Leading = 1,
+
+        /// <summary>
+        ///     The return enumeration will contain a trailing element.
+        /// </summary>
+        Trailing = 2
+    }
+}
