1ba0a06167740a5b4707daa0c2fda498f77d932d
Lesair Valmont <ixra.net@gmail.com>
2020-09-29 08:27:02 -0500

    ValidateMatchingParentheses unit tests.

---
+++ tests/CSharp-unit-tests/Challenges/MatchingParenthesesValidation.cs
@@ -0,0 +1,88 @@
+﻿using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class MatchingParenthesesValidation : BaseTest
+    {
+        public MatchingParenthesesValidation()
+        {
+            TypeToTest = typeof(ValidateMatchingParentheses);
+        }
+
+        private void TestImplementations(string s, bool expectedResult)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var actualResult = (bool) implementation.Invoke(null, new object[] {s});
+                actualResult.ShouldBe(expectedResult);
+            }
+        }
+
+        [Fact]
+        public void ReturnsFalseForNonMatchingParenthesisTestCase01()
+        {
+            var s = "(]";
+            const bool expectedResult = false;
+            TestImplementations(s, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsFalseForNonMatchingParenthesisTestCase02()
+        {
+            var s = "([)]";
+            const bool expectedResult = false;
+            TestImplementations(s, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsFalseForNonMatchingParenthesisTestCase03()
+        {
+            var s = "{{{(([]))}}";
+            const bool expectedResult = false;
+            TestImplementations(s, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsTrueForAnEmptyString()
+        {
+            var s = string.Empty;
+            const bool expectedResult = true;
+            TestImplementations(s, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsTrueForValidMatchingParenthesisTestCase01()
+        {
+            var s = "()";
+            const bool expectedResult = true;
+            TestImplementations(s, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsTrueForValidMatchingParenthesisTestCase02()
+        {
+            var s = "()[]{}";
+            const bool expectedResult = true;
+            TestImplementations(s, expectedResult);
+        }
+
+
+        [Fact]
+        public void ReturnsTrueForValidMatchingParenthesisTestCase03()
+        {
+            var s = "{[]}";
+            const bool expectedResult = true;
+            TestImplementations(s, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsTrueForValidMatchingParenthesisTestCase04()
+        {
+            var s = "[[({})]]{{{{((([[]])))}}}}([{}])";
+            const bool expectedResult = true;
+            TestImplementations(s, expectedResult);
+        }
+    }
+}
