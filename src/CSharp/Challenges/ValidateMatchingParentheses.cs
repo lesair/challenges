5397d7ed5f52ceb6b5b61484d7c44b83b10d2a8b
Lesair Valmont <ixra.net@gmail.com>
2020-09-29 19:06:10 -0500

    Code cleanup.
    Remove unused comment.

--- src/CSharp/Challenges/ValidateMatchingParentheses.cs
+++ src/CSharp/Challenges/ValidateMatchingParentheses.cs
@@ -7,7 +7,6 @@ namespace CSharp.Challenges
     ///     Validates if a string of brackets contains a valid combination of opening and closing bracket sequences.
     ///     Source: LeetCode
     ///     https://leetcode.com/problems/valid-parentheses/
-    ///     Source:
     /// </summary>
     public static class ValidateMatchingParentheses
     {
