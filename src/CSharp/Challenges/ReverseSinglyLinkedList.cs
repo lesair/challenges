caf8be251ee882e84fe9ce3d1bbab236a5d6d126
Lesair Valmont <ixra.net@gmail.com>
2020-09-30 10:02:09 -0500

    ReverseSinglyLinkedList challenge.

---
+++ src/CSharp/Challenges/ReverseSinglyLinkedList.cs
@@ -0,0 +1,34 @@
+﻿using CSharp.Library.SinglyLinkedList;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Reverses a singly linked list.
+    ///     Source: LeetCode
+    ///     https://leetcode.com/problems/reverse-linked-list/
+    /// </summary>
+    public static class ReverseSinglyLinkedList<TData>
+    {
+        /// <summary>
+        ///     Iterative.
+        ///     Time complexity: O(n).
+        ///     Space complexity: O(n).
+        /// </summary>
+        public static Node<TData> IterativeImplementation(Node<TData> currentNode)
+        {
+            Node<TData> previousNode = null;
+            var nextNode = currentNode?.Next;
+
+            while (currentNode != null)
+            {
+                currentNode.Next = previousNode;
+
+                previousNode = currentNode;
+                currentNode = nextNode;
+                nextNode = currentNode?.Next;
+            }
+
+            return previousNode;
+        }
+    }
+}
