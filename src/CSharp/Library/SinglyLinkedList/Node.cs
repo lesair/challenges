856935d6053f56ac5a8e13a4c9f17ba98f94aa5d
Lesair Valmont <ixra.net@gmail.com>
2020-09-30 10:00:54 -0500

    Singly linked list data structure.

---
+++ src/CSharp/Library/SinglyLinkedList/Node.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharp.Library.SinglyLinkedList
+{
+    /// <summary>
+    ///     A linked list node.
+    /// </summary>
+    /// <typeparam name="TData">Node data type.</typeparam>
+    public sealed class Node<TData>
+    {
+        public Node()
+        {
+        }
+
+        public Node(TData data)
+        {
+            Data = data;
+        }
+
+        public TData Data { get; set; }
+
+        public Node<TData> Next { get; set; }
+
+        public override string ToString()
+        {
+            var nextData = Next != null
+                ? $" ({Next.Data})"
+                : string.Empty;
+            return $"{Data}{nextData}";
+        }
+    }
+}
