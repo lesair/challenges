335e0af1a0a7052b2b90984515b6df63ad911af9
Lesair Valmont <ixra.net@gmail.com>
2020-10-01 17:59:04 -0500

    SumMultiplesOf3Or5 unit tests.

---
+++ tests/CSharp-unit-tests/Challenges/MultiplesOf3Or5Summation.cs
@@ -0,0 +1,55 @@
+﻿using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class MultiplesOf3Or5Summation : BaseTest
+    {
+        public MultiplesOf3Or5Summation()
+        {
+            TypeToTest = typeof(SumMultiplesOf3Or5);
+        }
+
+        private void TestImplementations(int n, long expectedResult)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var actualResult = (long) implementation.Invoke(null, new object[] {n});
+                actualResult.ShouldBe(expectedResult);
+            }
+        }
+
+        [Fact]
+        public void CorrectlyCalculatesTheSumOfMultiplesOf3Or5Below10()
+        {
+            const int n = 10;
+            const long expected = 23;
+            TestImplementations(n, expected);
+        }
+
+        [Fact]
+        public void CorrectlyCalculatesTheSumOfMultiplesOf3Or5Below100()
+        {
+            const int n = 100;
+            const long expected = 2318;
+            TestImplementations(n, expected);
+        }
+
+        [Fact]
+        public void CorrectlyCalculatesTheSumOfMultiplesOf3Or5Below1000()
+        {
+            const int n = 1000;
+            const long expected = 233168;
+            TestImplementations(n, expected);
+        }
+
+        [Fact]
+        public void CorrectlyCalculatesTheSumOfMultiplesOf3Or5Below1000000000()
+        {
+            const int n = 1000000000;
+            const long expected = 233333333166666668;
+            TestImplementations(n, expected);
+        }
+    }
+}
