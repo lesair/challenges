7cd9894176445a1e2ed1eb8f79cc9f5d537ac330
Lesair Valmont <ixra.net@gmail.com>
2020-10-02 18:00:05 -0500

    DetermineIfLinkedListHasACycle unit tests.

---
+++ tests/CSharp-unit-tests/Challenges/LinkedListCycleSearch.cs
@@ -0,0 +1,53 @@
+﻿using CSharp.Challenges;
+using CSharp.Library.SinglyLinkedList;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class LinkedListCycleSearch : BaseTest
+    {
+        public LinkedListCycleSearch()
+        {
+            TypeToTest = typeof(DetermineIfLinkedListHasACycle);
+        }
+
+        private void TestImplementations(Node<int> head, bool expectedResult)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var actualResult = (bool) implementation.Invoke(null, new object[] {head});
+                actualResult.ShouldBe(expectedResult);
+            }
+        }
+
+        [Fact]
+        public void ReturnsFalseForASingleNodeLinkedList()
+        {
+            var linkedListManager = new SinglyLinkedListManager<int>();
+            var headNode = linkedListManager.Create(new[] {1});
+            const bool expectedResult = false;
+            TestImplementations(headNode, expectedResult);
+        }
+
+
+        [Fact]
+        public void ReturnsFalseWhenThereAreNoCycles()
+        {
+            var linkedListManager = new SinglyLinkedListManager<int>();
+            var headNode = linkedListManager.Create(new[] {3, 2, 0, -4});
+            const bool expectedResult = false;
+            TestImplementations(headNode, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsTrueWhenThereIsACycle()
+        {
+            var linkedListManager = new SinglyLinkedListManager<int>();
+            var headNode = linkedListManager.Create(new[] {3, 2, 0, -4});
+            linkedListManager["-4"].Next = linkedListManager["2"];
+            const bool expectedResult = true;
+            TestImplementations(headNode, expectedResult);
+        }
+    }
+}
