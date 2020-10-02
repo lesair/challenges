5a7abe6a82fc79d2b1488604d611ac132d03b452
Lesair Valmont <ixra.net@gmail.com>
2020-10-02 17:59:57 -0500

    DetermineIfLinkedListHasACycle challenge.

---
+++ src/CSharp/Challenges/DetermineIfLinkedListHasACycle.cs
@@ -0,0 +1,42 @@
+﻿using CSharp.Library.SinglyLinkedList;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Given the head of a linked list, determine if the linked list has a cycle in it.
+    ///     Source: LeetCode
+    ///     https://leetcode.com/problems/linked-list-cycle/
+    ///     Source: HackerRank
+    ///     https://youtu.be/MFOAbpfrJ8g
+    ///     Source: Alan Chávez
+    ///     https://youtu.be/0fpcIuvcRI0?t=211
+    /// </summary>
+    public static class DetermineIfLinkedListHasACycle
+    {
+        /// <summary>
+        ///     Iterative.
+        ///     Time complexity: O(n).
+        ///     Space complexity: O(n).
+        /// </summary>
+        public static bool Implementation(Node<int> head)
+        {
+            var fastTraverse = head;
+            var slowTraverse = head;
+            var timing = 0;
+            while (fastTraverse != null && slowTraverse != null)
+            {
+                fastTraverse = fastTraverse.Next;
+                if (timing > 0 && timing % 2 == 0)
+                {
+                    slowTraverse = slowTraverse.Next;
+                    if (fastTraverse == slowTraverse)
+                        return true;
+                }
+
+                timing++;
+            }
+
+            return false;
+        }
+    }
+}
