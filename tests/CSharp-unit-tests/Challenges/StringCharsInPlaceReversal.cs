9e1f768c1d8440edf65c80f645165955cbbdf043
Lesair Valmont <ixra.net@gmail.com>
2020-10-08 08:53:51 -0500

    ReverseStringCharsInPlace unit tests.

---
+++ tests/CSharp-unit-tests/Challenges/StringCharsInPlaceReversal.cs
@@ -0,0 +1,33 @@
+﻿using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class StringCharsInPlaceReversal : BaseTest
+    {
+        public StringCharsInPlaceReversal()
+        {
+            TypeToTest = typeof(ReverseStringCharsInPlace);
+        }
+
+        private void TestImplementations(string originalString, string expectedResult)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var s = new string(originalString);
+                implementation.Invoke(null, new object[] {s});
+                s.ShouldBe(expectedResult);
+                s.ShouldNotBe(originalString);
+            }
+        }
+
+        [Fact]
+        public void ReturnsAUniformlyShuffledList()
+        {
+            var s = "steal pound cake";
+            const string expected = "ekac dnuop laets";
+            TestImplementations(s, expected);
+        }
+    }
+}
