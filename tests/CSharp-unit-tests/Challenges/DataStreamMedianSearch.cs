f492655f87923dc354d4b2f7067e162a1509cbc1
Lesair Valmont <ixra.net@gmail.com>
2020-10-12 17:42:17 -0500

    MedianFinder unit tests.

---
+++ tests/CSharp-unit-tests/Challenges/DataStreamMedianSearch.cs
@@ -0,0 +1,35 @@
+﻿using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class DataStreamMedianSearch
+    {
+        [Fact]
+        public void ReturnsTheExpectedMedians()
+        {
+            var medianFinder = new MedianFinder();
+            medianFinder.AddNum(1);
+            medianFinder.AddNum(2);
+            medianFinder.FindMedian().ShouldBe(1.5);
+            medianFinder.AddNum(3);
+            medianFinder.FindMedian().ShouldBe(2);
+        }
+
+        [Fact]
+        public void ReturnsTheExpectedMediansForDuplicatedValues()
+        {
+            var medianFinder = new MedianFinder();
+            medianFinder.AddNum(2);
+            medianFinder.AddNum(2);
+            medianFinder.AddNum(2);
+            medianFinder.AddNum(3);
+            medianFinder.AddNum(3);
+            medianFinder.FindMedian().ShouldBe(2);
+            medianFinder.AddNum(3);
+            medianFinder.AddNum(3);
+            medianFinder.FindMedian().ShouldBe(3);
+        }
+    }
+}
