496376e8b36940b29644dc7c4e79f121ed9e7b7c
Lesair Valmont <ixra.net@gmail.com>
2020-11-17 18:38:39 -0600

    FindSmallestPositiveInteger challenge.

---
+++ src/CSharp/Challenges/FindSmallestPositiveInteger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Return the smallest positive integer (greater than 0) that does not occur in a given array.
+    ///     Source: Codility
+    ///     Codility demo test.
+    /// </summary>
+    public static class FindSmallestPositiveInteger
+    {
+        /// <summary>
+        ///     Iterative.
+        ///     Time complexity: O(n log n).
+        ///     Space complexity: O(1).
+        /// </summary>
+        public static int IterativeImplementation(int[] integers)
+        {
+            var result = 1;
+            Array.Sort(integers);
+
+            foreach (var i in integers.Distinct())
+                if (result == i)
+                    result++;
+
+            return result;
+        }
+    }
+}
