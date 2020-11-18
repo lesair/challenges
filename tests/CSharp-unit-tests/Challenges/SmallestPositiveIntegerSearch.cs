cfcba21d66ec6dbcbdad82b94c9f718e57dd589a
Lesair Valmont <ixra.net@gmail.com>
2020-11-17 18:38:48 -0600

    FindSmallestPositiveInteger unit tests.

---
+++ tests/CSharp-unit-tests/Challenges/SmallestPositiveIntegerSearch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class SmallestPositiveIntegerSearch : BaseTest
+    {
+        public SmallestPositiveIntegerSearch()
+        {
+            TypeToTest = typeof(FindSmallestPositiveInteger);
+        }
+
+        private void TestImplementations(IEnumerable<int> integers, int expectedResult)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var actualResult = (int) implementation.Invoke(null, new object[] {integers});
+                actualResult.ShouldBe(expectedResult);
+            }
+        }
+
+        [Fact]
+        public void CorrectlyReturnsMissingSmallestPositiveInteger()
+        {
+            var integers = new[] {1, 3, 6, 4, 1, 2};
+            const int expected = 5;
+            TestImplementations(integers, expected);
+        }
+    }
+}
