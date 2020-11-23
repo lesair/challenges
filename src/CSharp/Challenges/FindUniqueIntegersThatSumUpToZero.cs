b982564ffee38cff09fe247b9a6ccb56ea020ded
Lesair Valmont <ixra.net@gmail.com>
2020-11-23 17:48:04 -0600

    FindUniqueIntegersThatSumUpToZero challenge.

---
+++ src/CSharp/Challenges/FindUniqueIntegersThatSumUpToZero.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Find N Unique Integers Sum up to Zero
+    ///     Source: LeetCode
+    ///     https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+    /// </summary>
+    public static class FindUniqueIntegersThatSumUpToZero
+    {
+        /// <summary>
+        ///     Iterative. Elegant.
+        ///     Hash set.
+        ///     Time complexity: O(n).
+        ///     Space complexity: O(n).
+        /// </summary>
+        public static int[] IterativeImplementation(int n)
+        {
+            HashSet<int> set;
+            if (n % 2 == 0)
+            {
+                set = new HashSet<int>(Enumerable.Range(-n / 2, n + 1));
+                set.Remove(0);
+            }
+            else
+            {
+                set = new HashSet<int>(Enumerable.Range(-n / 2, n));
+            }
+
+            return set.ToArray();
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/IntegersThatSumUpToZeroSearch.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class IntegersThatSumUpToZeroSearch : BaseTest
+    {
+        public IntegersThatSumUpToZeroSearch()
+        {
+            TypeToTest = typeof(FindUniqueIntegersThatSumUpToZero);
+        }
+
+        private void TestImplementations(int n)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var actualResult = (int[]) implementation.Invoke(null, new object[] {n});
+                actualResult.ShouldNotBeNull();
+                actualResult.Sum().ShouldBe(0);
+                actualResult.Length.ShouldBe(n);
+                actualResult.ShouldBeUnique();
+            }
+        }
+
+        [Fact]
+        public void ReturnsEvenUniqueIntegersThatSumUpToZero()
+        {
+            const int n = 4;
+            TestImplementations(n);
+        }
+
+        [Fact]
+        public void ReturnsManyUniqueIntegersThatSumUpToZero()
+        {
+            const int n = 100;
+            TestImplementations(n);
+        }
+
+        [Fact]
+        public void ReturnsOddUniqueIntegersThatSumUpToZero()
+        {
+            const int n = 5;
+            TestImplementations(n);
+        }
+    }
+}
