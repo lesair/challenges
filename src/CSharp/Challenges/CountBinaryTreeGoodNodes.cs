0f4525951b860d2d71606ef0cf83494ff438f7b2
Lesair Valmont <ixra.net@gmail.com>
2020-11-23 17:44:43 -0600

    CountBinaryTreeGoodNodes challenge.

---
+++ src/CSharp/Challenges/CountBinaryTreeGoodNodes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using CSharp.Library.Tree;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Count the number of good nodes in the binary tree.
+    ///     A node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+    ///     Source: LeetCode
+    ///     https://leetcode.com/problems/count-good-nodes-in-binary-tree/submissions/
+    /// </summary>
+    public static class CountBinaryTreeGoodNodes
+    {
+        /// <summary>
+        ///     Recursive. Pre-orderly binary tree traversal.
+        ///     Time complexity: O(n).
+        ///     Space complexity: O(h).
+        /// </summary>
+        public static int RecursiveImplementation(BinaryNode<int> tree)
+        {
+            var counter = 0;
+
+            void TraverseTree(BinaryNode<int> node, int max)
+            {
+                if (node == null)
+                    return;
+
+                if (node.Data >= max)
+                {
+                    counter++;
+                    max = Math.Max(max, node.Data);
+                }
+
+                TraverseTree(node.Left, max);
+                TraverseTree(node.Right, max);
+            }
+
+            TraverseTree(tree, tree.Data);
+
+            return counter;
+        }
+
+        // TODO: Iterative implementation.
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/BinaryTreeGoodNodesCounting.cs
@@ -0,0 +1,41 @@
+﻿using CSharp.Challenges;
+using CSharp.Library.Tree;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class BinaryTreeGoodNodesCounting : BaseTest
+    {
+        public BinaryTreeGoodNodesCounting()
+        {
+            TypeToTest = typeof(CountBinaryTreeGoodNodes);
+        }
+
+        private void TestImplementations(BinaryNode<int> rootNode, int expectedResult)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var actualResult = (int?) implementation.Invoke(null, new object[] {rootNode});
+                actualResult.ShouldNotBeNull();
+                actualResult.ShouldBe(expectedResult);
+            }
+        }
+
+        [Fact]
+        public void CorrectlyCountsGoodNodesTestCase01()
+        {
+            const int expectedResult = 4;
+            var binaryTree = BinaryTreeManager.Create(new int?[] {5, 3, 10, 20, 21, 1, null});
+            TestImplementations(binaryTree.Root, expectedResult);
+        }
+
+        [Fact]
+        public void CorrectlyCountsGoodNodesTestCase02()
+        {
+            const int expectedResult = 2;
+            var binaryTree = BinaryTreeManager.Create(new int?[] {8, 2, 6, 8, 7});
+            TestImplementations(binaryTree.Root, expectedResult);
+        }
+    }
+}
