ac65a2ae7ec34b0e7b967ef4072874867d6f0eda
Lesair Valmont <ixra.net@gmail.com>
2020-12-08 17:39:15 -0600

    ReverseWordsInPlace challenge.

---
+++ src/CSharp/Challenges/ReverseWordsInPlace.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Given a collection of characters, reverse the order of the words in place.
+    ///     Source: Interview Cake.
+    ///     https://www.interviewcake.com/question/csharp/reverse-words
+    ///     Source: LeetCode.
+    ///     https://leetcode.com/problems/reverse-words-in-a-string/
+    /// </summary>
+    public static class ReverseWordsInPlace
+    {
+        /// <summary>
+        ///     Iterative.
+        ///     Time complexity: O(n).
+        ///     Space complexity: O(1).
+        /// </summary>
+        public static void IterativeImplementation(char[] chars)
+        {
+            void Swap(int start, int end)
+            {
+                while (++start < --end)
+                {
+                    chars[start] = (char) (chars[start] ^ chars[end]);
+                    chars[end] = (char) (chars[end] ^ chars[start]);
+                    chars[start] = (char) (chars[start] ^ chars[end]);
+                }
+            }
+
+            // First step: full char reversal.
+            var i = 0;
+            var j = chars.Length;
+            Swap(i - 1, j);
+
+            // Second step: char reversal by word.
+            i = 0;
+            while (i < chars.Length)
+            {
+                j = Array.IndexOf(chars, ' ', i);
+                if (j == -1)
+                    j = chars.Length;
+                Swap(i - 1, j);
+                i = j + 1;
+            }
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/WordsInPlaceReversal.cs
@@ -0,0 +1,64 @@
+﻿using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class WordsInPlaceReversal : BaseTest
+    {
+        public WordsInPlaceReversal()
+        {
+            TypeToTest = typeof(ReverseWordsInPlace);
+        }
+
+        private void TestImplementations(char[] chars, string expected)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                implementation.Invoke(null, new object[] {chars});
+                var actual = new string(chars);
+                actual.ShouldBe(expected);
+            }
+        }
+
+        [Fact]
+        public void CorrectlyReversesWordsTestCase01()
+        {
+            var chars = new[] {'a', 'b', ' ', 'c', 'd'};
+            const string expected = "cd ab";
+            TestImplementations(chars, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReversesWordsTestCase02()
+        {
+            var chars = new[] {'c', 'a', 'k', 'e', ' ', 'p', 'o', 'u', 'n', 'd', ' ', 's', 't', 'e', 'a', 'l'};
+            const string expected = "steal pound cake";
+            TestImplementations(chars, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReversesASingleWord()
+        {
+            var chars = "Hello".ToCharArray();
+            const string expected = "Hello";
+            TestImplementations(chars, expected);
+        }
+
+
+        [Fact]
+        public void CorrectlyReversesASingleChar()
+        {
+            var chars = new[] {'a'};
+            const string expected = "a";
+            TestImplementations(chars, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReversesAnEmptyCharArray()
+        {
+            var chars = new char[0];
+            TestImplementations(chars, string.Empty);
+        }
+    }
+}
