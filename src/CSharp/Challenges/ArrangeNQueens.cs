af7b56942242201d556efa78c435258cf80ee7e2
Lesair Valmont <ixra.net@gmail.com>
2021-03-29 18:29:10 -0600

    ArrangeNQueens challenge.

---
+++ src/CSharp/Challenges/ArrangeNQueens.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Place n chess queens on an n×n chessboard so that no two queens threaten each other.
+    ///     Source: Wikipedia
+    ///     https://en.wikipedia.org/wiki/Eight_queens_puzzle
+    ///     Source: Abdul Bari
+    ///     https://youtu.be/xFv_Hl4B83A
+    ///     Source: LeetCode
+    ///     https://leetcode.com/problems/n-queens/
+    /// </summary>
+    public static class ArrangeNQueens
+    {
+        private static int _n;
+
+        public static IEnumerable<IEnumerable<string>> Implementation(int n)
+        {
+            _n = n;
+            foreach (var validBoard in Backtracking()) yield return validBoard.ToStrings();
+        }
+
+        private static IEnumerable<IEnumerable<int>> Backtracking(IReadOnlyCollection<int> board = null)
+        {
+            board ??= new HashSet<int>();
+
+            if (board.Count == _n)
+            {
+                yield return board;
+                yield break;
+            }
+
+            for (var i = 0; i < _n; i++)
+            {
+                var newBoard = new HashSet<int>(board);
+                if (!newBoard.Add(i) || newBoard.ToArray().ContainsQueensInDiagonal()) continue;
+                foreach (var result in Backtracking(newBoard)) yield return result;
+            }
+        }
+
+        private static bool ContainsQueensInDiagonal(this IReadOnlyList<int> board)
+        {
+            if (board.Count < 2)
+                return false;
+
+            var lastY = board.Count - 1;
+            var lastX = board[lastY];
+
+            for (var currentY = 0; currentY < board.Count - 1; currentY++)
+            {
+                var currentX = board[currentY];
+                if (Math.Abs(lastX - currentX) - Math.Abs(lastY - currentY) == 0)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static IEnumerable<string> ToStrings(this IEnumerable<int> board)
+        {
+            foreach (var row in board)
+            {
+                var stringRepresentation = Enumerable.Repeat('.', _n).ToArray();
+                stringRepresentation[row] = 'Q';
+                yield return new string(stringRepresentation);
+            }
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/QueensArranger.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class QueensArranger : BaseTest
+    {
+        public QueensArranger()
+        {
+            TypeToTest = typeof(ArrangeNQueens);
+        }
+
+        private void TestImplementations(int n, ICollection<IEnumerable<string>> expectedResults)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var actualResults = ((IEnumerable<IEnumerable<string>>) implementation.Invoke(null, new object[] {n}))
+                    ?.ToList();
+                actualResults.ShouldNotBeNull();
+                actualResults.ShouldBe(expectedResults);
+            }
+        }
+
+        private void TestImplementations(int n, int expectedResultsCount)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var actualResults = ((IEnumerable<IEnumerable<string>>) implementation.Invoke(null, new object[] {n}))
+                    ?.ToList();
+                actualResults.ShouldNotBeNull();
+                actualResults.Count.ShouldBe(expectedResultsCount);
+            }
+        }
+
+        [Fact]
+        public void GeneratesAllValidSolutionsForOneQueen()
+        {
+            const int n = 1;
+            const int expectedResultsCount = 1;
+            var solution1 = new List<string> {"Q"};
+            var expectedResults = new List<IEnumerable<string>> {solution1};
+            TestImplementations(n, expectedResults);
+            TestImplementations(n, expectedResultsCount);
+        }
+
+        [Fact]
+        public void GeneratesAllValidSolutionsForTwoQueens()
+        {
+            const int n = 2;
+            const int expectedResultsCount = 0;
+            var expectedResults = new List<IEnumerable<string>>();
+            TestImplementations(n, expectedResults);
+            TestImplementations(n, expectedResultsCount);
+        }
+
+        [Fact]
+        public void GeneratesAllValidSolutionsForThreeQueens()
+        {
+            const int n = 3;
+            const int expectedResultsCount = 0;
+            var expectedResults = new List<IEnumerable<string>>();
+            TestImplementations(n, expectedResults);
+            TestImplementations(n, expectedResultsCount);
+        }
+
+        [Fact]
+        public void GeneratesAllValidSolutionsForFourQueens()
+        {
+            const int n = 4;
+            const int expectedResultsCount = 2;
+            var solution1 = new List<string>
+            {
+                ".Q..",
+                "...Q",
+                "Q...",
+                "..Q."
+            };
+            var solution2 = new List<string>
+            {
+                "..Q.",
+                "Q...",
+                "...Q",
+                ".Q.."
+            };
+            var expectedResults = new List<IEnumerable<string>> {solution1, solution2};
+            TestImplementations(n, expectedResults);
+            TestImplementations(n, expectedResultsCount);
+        }
+
+        [Fact]
+        public void GeneratesAllValidSolutionsForFiveQueens()
+        {
+            const int n = 5;
+            const int expectedResultsCount = 10;
+            TestImplementations(n, expectedResultsCount);
+        }
+
+        [Fact]
+        public void GeneratesAllValidSolutionsForElevenQueens()
+        {
+            const int n = 11;
+            const int expectedResultsCount = 2_680;
+            TestImplementations(n, expectedResultsCount);
+        }
+    }
+}
