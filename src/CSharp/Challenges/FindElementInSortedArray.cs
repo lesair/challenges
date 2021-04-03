555596888d9dc95406a2f285d0f862ce5240c4d4
Lesair Valmont <ixra.net@gmail.com>
2021-04-03 17:24:27 -0600

    FindElementInSortedArray challenge.

---
+++ src/CSharp/Challenges/FindElementInSortedArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Source: LeetCode
+    ///     https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+    /// </summary>
+    public static class FindElementInSortedArray
+    {
+        /// <summary>
+        ///     Iterative.
+        ///     Time complexity: O(log n)
+        ///     Space complexity: O(1).
+        /// </summary>
+        public static int BinarySearchImplementation(int[] numbers, int target)
+        {
+            var l = 0;
+            var r = numbers.Length - 1;
+
+            while (l <= r)
+            {
+                var m = (l + r) / 2;
+                if (target == numbers[m])
+                    return m;
+
+                if (target < numbers[m])
+                    r = m - 1;
+                else
+                    l = m + 1;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        ///     Iterative.
+        ///     Time complexity: O(log n)
+        ///     https://docs.microsoft.com/en-us/dotnet/api/system.array.binarysearch?view=net-5.0
+        /// </summary>
+        public static int BuiltInBinarySearchImplementation(int[] numbers, int target)
+        {
+            return Array.BinarySearch(numbers, target);
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/SortedArrayElementSearch.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class SortedArrayElementSearch : BaseTest
+    {
+        public SortedArrayElementSearch()
+        {
+            TypeToTest = typeof(FindElementInSortedArray);
+        }
+
+        private void TestImplementations(IEnumerable<int> numbers, int target, int expected)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var invocationResult = (int?) implementation.Invoke(null, new object[] {numbers, target});
+                invocationResult.ShouldNotBeNull();
+                var actual = invocationResult.Value;
+                if (expected >= 0)
+                    actual.ShouldBe(expected);
+                else
+                    actual.ShouldBeLessThan(0);
+            }
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheIndexOfTheMatchedElementTestCase01()
+        {
+            var numbers = new[] {5};
+            const int target = 5;
+            const int expected = 0;
+            TestImplementations(numbers, target, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheIndexOfTheMatchedElementTestCase02()
+        {
+            var numbers = new[] {2, 5};
+            const int target = 2;
+            const int expected = 0;
+            TestImplementations(numbers, target, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheIndexOfTheMatchedElementTestCase03()
+        {
+            var numbers = new[] {2, 5};
+            const int target = 5;
+            const int expected = 1;
+            TestImplementations(numbers, target, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheIndexOfTheMatchedElementTestCase04()
+        {
+            var numbers = new[] {-1, 0, 3, 5, 9, 12};
+            const int target = 9;
+            const int expected = 4;
+            TestImplementations(numbers, target, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsNotFoundTestCase01()
+        {
+            var numbers = new[] {5};
+            const int target = 6;
+            const int expected = -1;
+            TestImplementations(numbers, target, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsNotFoundTestCase02()
+        {
+            var numbers = new[] {-1, 0, 3, 5, 9, 12};
+            const int target = 2;
+            const int expected = -1;
+            TestImplementations(numbers, target, expected);
+        }
+    }
+}
