83d290f45315e856577f35e44b53c21191e0d254
Lesair Valmont <ixra.net@gmail.com>
2021-04-03 17:24:49 -0600

    FindMinimumInRotatedSortedArray challenge.

---
+++ src/CSharp/Challenges/FindMinimumInRotatedSortedArray.cs
@@ -0,0 +1,47 @@
+﻿namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Source: LeetCode
+    ///     https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+    /// </summary>
+    public static class FindMinimumInRotatedSortedArray
+    {
+        /// <summary>
+        ///     Recursive.
+        ///     Time complexity: O(log n)
+        ///     Space complexity: O(n).
+        /// </summary>
+        public static int RecursiveImplementation(int[] numbers)
+        {
+            if (numbers[0] <= numbers[^1])
+                return numbers[0];
+
+            var m = numbers.Length / 2 - 1;
+            return RecursiveImplementation(numbers[0] <= numbers[m]
+                ? numbers[(m + 1)..numbers.Length]
+                : numbers[..(m + 1)]);
+        }
+
+        /// <summary>
+        ///     Iterative.
+        ///     Time complexity: O(log n)
+        ///     Space complexity: O(1).
+        /// </summary>
+        public static int IterativeImplementation(int[] numbers)
+        {
+            var l = 0;
+            var r = numbers.Length - 1;
+
+            while (numbers[l] > numbers[r])
+            {
+                var m = (l + r) / 2;
+                if (numbers[l] <= numbers[m])
+                    l = m + 1;
+                else
+                    r = m;
+            }
+
+            return numbers[l];
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/RotatedSortedArrayMinimumSearch.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Linq;
+using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class RotatedSortedArrayMinimumSearch : BaseTest
+    {
+        public RotatedSortedArrayMinimumSearch()
+        {
+            TypeToTest = typeof(FindMinimumInRotatedSortedArray);
+        }
+
+        private void TestImplementations(IEnumerable numbers, int expected)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var actual = (int?) implementation.Invoke(null, new object[] {numbers});
+                actual.ShouldNotBeNull();
+                actual.ShouldBe(expected);
+            }
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheMinimumValueTestCase01()
+        {
+            var numbers = new[] {3, 4, 5, 1, 2};
+            const int expected = 1;
+            TestImplementations(numbers, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheMinimumValueTestCase02()
+        {
+            var numbers = new[] {4, 5, 6, 7, 0, 1, 2};
+            const int expected = 0;
+            TestImplementations(numbers, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheMinimumValueTestCase03()
+        {
+            var numbers = new[] {11, 13, 15, 17};
+            const int expected = 11;
+            TestImplementations(numbers, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheMinimumValueTestCase04()
+        {
+            var numbers = new[] {1, 2};
+            const int expected = 1;
+            TestImplementations(numbers, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheMinimumValueTestCase05()
+        {
+            var numbers = new[] {2, 1};
+            const int expected = 1;
+            TestImplementations(numbers, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheMinimumValueTestCase06()
+        {
+            var numbers = new[] {5, 1, 2, 3, 4};
+            const int expected = 1;
+            TestImplementations(numbers, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheMinimumValueTestCase07()
+        {
+            var numbers = new[] {3, 1, 2};
+            const int expected = 1;
+            TestImplementations(numbers, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheMinimumValueInLogNTime()
+        {
+            var numbers = Enumerable.Range(-500_000, 1_000_000).ToArray();
+            numbers = numbers.Union(Enumerable.Range(-1_000_000, 500_000)).ToArray();
+            const int expected = -1_000_000;
+            TestImplementations(numbers, expected);
+        }
+    }
+}
