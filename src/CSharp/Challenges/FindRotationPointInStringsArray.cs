7dfdfccc3f50260b84a439d632717e1e41df8c1f
Lesair Valmont <ixra.net@gmail.com>
2021-04-06 08:16:26 -0500

    FindRotationPointInStringsArray challenge.

---
+++ src/CSharp/Challenges/FindRotationPointInStringsArray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Source: Interview Cake.
+    ///     https://www.interviewcake.com/question/csharp/find-rotation-point
+    /// </summary>
+    public static class FindRotationPointInStringsArray
+    {
+        /// <summary>
+        ///     Iterative.
+        ///     Time complexity: O(ℓ + log n).
+        ///     Some languages —like German, Russian, and Dutch— can have arbitrarily long words, so we might want to factor the
+        ///     length of the words into our runtime. We could say the length of the words is ℓ, each string comparison takes
+        ///     O(ℓ) time, therefore the whole algorithm takes O(ℓ + log n) time. The longest word in English is
+        ///     pneumonoultramicroscopicsilicovolcanoconiosis, a medical term. It's 45 letters long, so for English we can assume
+        ///     our word lengths are bounded by some constant so we'll say the string comparison takes constant time, making it
+        ///     O(log n) time.
+        ///     Space complexity: O(1).
+        /// </summary>
+        public static int IterativeBinarySearchImplementation1(string[] strings)
+        {
+            var l = 0;
+            var r = strings.Length - 1;
+            var m = r / 2;
+
+            while (l <= r)
+            {
+                if (string.Compare(strings[l], strings[r], StringComparison.OrdinalIgnoreCase) <= 0)
+                    return l;
+
+                m = (l + r) / 2;
+                if (string.Compare(strings[l], strings[m], StringComparison.OrdinalIgnoreCase) <= 0)
+                    l = m + 1;
+                else
+                    r = m;
+            }
+
+            return m;
+        }
+
+        /// <summary>
+        ///     Iterative.
+        ///     Time complexity: O(ℓ + log n).
+        ///     Space complexity: O(1).
+        /// </summary>
+        public static int IterativeBinarySearchImplementation2(string[] strings)
+        {
+            var l = 0;
+            var r = strings.Length - 1;
+
+            while (string.Compare(strings[l], strings[r], StringComparison.OrdinalIgnoreCase) > 0)
+            {
+                var m = (l + r) / 2;
+                if (string.Compare(strings[l], strings[m], StringComparison.OrdinalIgnoreCase) <= 0)
+                    l = m + 1;
+                else
+                    r = m;
+            }
+
+            return l;
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/RotationPointInStringsArraySearch.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class RotationPointInStringsArraySearch : BaseTest
+    {
+        public RotationPointInStringsArraySearch()
+        {
+            TypeToTest = typeof(FindRotationPointInStringsArray);
+        }
+
+        private void TestImplementations(IEnumerable<string> strings, int expected)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var invocationResult = (int?) implementation.Invoke(null, new object[] {strings});
+                invocationResult.ShouldNotBeNull();
+                var actual = invocationResult.Value;
+                actual.ShouldBe(expected);
+            }
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheRotationPointIndexTestCase01()
+        {
+            var strings = new[]
+            {
+                "ptolemaic",
+                "retrograde",
+                "supplant",
+                "undulate",
+                "xenoepist",
+                "asymptote", // <-- rotates here!
+                "babka",
+                "banoffee",
+                "engender",
+                "karpatka",
+                "othellolagkage"
+            };
+            const int expected = 5;
+            TestImplementations(strings, expected);
+        }
+
+
+        [Fact]
+        public void CorrectlyReturnsTheRotationPointIndexTestCase02()
+        {
+            var strings = new[]
+            {
+                "ptolemaic"
+            };
+            const int expected = 0;
+            TestImplementations(strings, expected);
+        }
+
+        [Fact]
+        public void CorrectlyReturnsTheRotationPointIndexTestCase03()
+        {
+            var strings = new[]
+            {
+                "asymptote",
+                "babka",
+                "banoffee",
+                "engender",
+                "karpatka",
+                "othellolagkage",
+                "ptolemaic",
+                "retrograde",
+                "supplant",
+                "undulate",
+                "xenoepist"
+            };
+            const int expected = 0;
+            TestImplementations(strings, expected);
+        }
+    }
+}
