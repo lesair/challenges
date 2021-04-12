36be4d422a92dd0786e2267702f9f51ca2d47bea
Lesair Valmont <ixra.net@gmail.com>
2021-04-12 18:10:31 -0500

    InvertBinaryTree challenge.

---
+++ src/CSharp/Challenges/InvertBinaryTree.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CSharp.Library.Tree;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Horizontally flip a binary tree given its root node.
+    ///     Source: LeetCode
+    ///     https://leetcode.com/problems/invert-binary-tree/
+    /// </summary>
+    public static class InvertBinaryTree
+    {
+        /// <summary>
+        ///     Iterative. Breadth-first search traversal.
+        ///     Time complexity: O(n).
+        ///     Space complexity: O(1).
+        /// </summary>
+        public static BinaryNode<int> IterativeImplementation(BinaryNode<int> root)
+        {
+            if (root == null)
+                return null;
+
+            var queue = new Queue<BinaryNode<int>>();
+            queue.Enqueue(root);
+            while (queue.Any())
+            {
+                var currentNode = queue.Dequeue();
+                if (currentNode.Right != null)
+                    queue.Enqueue(currentNode.Right);
+                if (currentNode.Left != null)
+                    queue.Enqueue(currentNode.Left);
+
+                (currentNode.Left, currentNode.Right) = (currentNode.Right, currentNode.Left);
+            }
+
+            return root;
+        }
+
+        /// <summary>
+        ///     Recursive. Elegant. Breadth-first search traversal.
+        ///     Time complexity: O(n).
+        ///     Space complexity: O(1).
+        /// </summary>
+        public static BinaryNode<int> RecursiveImplementation(BinaryNode<int> root)
+        {
+            if (root == null)
+                return null;
+
+            (root.Left, root.Right) = (root.Right, root.Left);
+            RecursiveImplementation(root.Left);
+            RecursiveImplementation(root.Right);
+
+            return root;
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/BinaryTreeInversion.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using CSharp.Challenges;
+using CSharp.Library.Extensions;
+using CSharp.Library.Tree;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class BinaryTreeInversion : BaseTest
+    {
+        public BinaryTreeInversion()
+        {
+            TypeToTest = typeof(InvertBinaryTree);
+        }
+
+        private void TestImplementations(IReadOnlyCollection<int?> nodesData, IReadOnlyCollection<int?> expectedResults)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var binaryTree = BinaryTreeManager.Create(nodesData);
+                var nodeActualResult = (BinaryNode<int>) implementation.Invoke(null, new object[] {binaryTree.Root});
+                var actualResults = new List<int?>();
+                TraverseBinaryTreeInBreadthFirstSearchWay<int>.Visit = node => actualResults.Add(node?.Data);
+                TraverseBinaryTreeInBreadthFirstSearchWay<int>.IterativeImplementation(nodeActualResult);
+                actualResults.TrimTrailingNulls().ShouldBe(expectedResults);
+            }
+        }
+
+        [Fact]
+        public void CorrectlyInvertsABinaryTreeTestCase01()
+        {
+            var nodesData = new int?[] {4, 2, 7, 1, 3, 6, 9};
+            var expectedResults = new int?[] {4, 7, 2, 9, 6, 3, 1};
+            TestImplementations(nodesData, expectedResults);
+        }
+
+        [Fact]
+        public void CorrectlyInvertsABinaryTreeTestCase02()
+        {
+            var nodesData = new int?[] {2, 1, 3};
+            var expectedResults = new int?[] {2, 3, 1};
+            TestImplementations(nodesData, expectedResults);
+        }
+
+        [Fact]
+        public void CorrectlyInvertsABinaryTreeTestCase03()
+        {
+            var nodesData = new int?[] {2, null, 3};
+            var expectedResults = new int?[] {2, 3};
+            TestImplementations(nodesData, expectedResults);
+        }
+
+        [Fact]
+        public void CorrectlyInvertsABinaryTreeTestCase04()
+        {
+            var nodesData = new int?[] {2, 1};
+            var expectedResults = new int?[] {2, null, 1};
+            TestImplementations(nodesData, expectedResults);
+        }
+
+        [Fact]
+        public void CorrectlyInvertsABinaryTreeTestCase05()
+        {
+            var nodesData = new int?[] {4, 2, null, 1};
+            var expectedResults = new int?[] {4, null, 2, null, 1};
+            TestImplementations(nodesData, expectedResults);
+        }
+
+        [Fact]
+        public void CorrectlyInvertsAnEmptyBinaryTree()
+        {
+            var nodesData = new int?[0];
+            var expectedResults = new int?[0];
+            TestImplementations(nodesData, expectedResults);
+        }
+    }
+}
