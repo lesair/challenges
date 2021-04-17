def6f9a197cd009aa0560cab5b884dc699f84022
Lesair Valmont <ixra.net@gmail.com>
2021-04-17 08:01:05 -0500

    FindClosestValueInBinarySearchTree challenge.

---
+++ src/CSharp/Challenges/FindClosestValueInBinarySearchTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using CSharp.Library.Tree;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Source: GeeksforGeeks.
+    ///     https://www.geeksforgeeks.org/find-closest-element-binary-search-tree/
+    /// </summary>
+    public static class FindClosestValueInBinarySearchTree
+    {
+        /// <summary>
+        ///     Recursive.
+        ///     Time complexity: O(h).
+        ///     Space complexity: O(h) (space in the call stack due to recursion).
+        /// </summary>
+        public static int RecursiveImplementation(BinaryNode<int> root, int target)
+        {
+            int FindClosestValue(BinaryNode<int> node, int? closestValueSoFar)
+            {
+                switch (node)
+                {
+                    case null when closestValueSoFar == null:
+                        throw new InvalidOperationException();
+                    case null:
+                        return closestValueSoFar.Value;
+                }
+
+                if (node.Data == target)
+                    return node.Data;
+
+                var nodeDifference = Math.Abs(node.Data - target);
+                if (closestValueSoFar.HasValue)
+                {
+                    var closestDifferenceSoFar = Math.Abs(closestValueSoFar.Value - target);
+                    closestValueSoFar = nodeDifference < closestDifferenceSoFar ? node.Data : closestValueSoFar.Value;
+                }
+                else
+                {
+                    closestValueSoFar = node.Data;
+                }
+
+                return FindClosestValue(target < node.Data ? node.Left : node.Right, closestValueSoFar);
+            }
+
+            return FindClosestValue(root, null);
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/BinarySearchTreeClosestValueSearch.cs
@@ -0,0 +1,155 @@
+﻿using CSharp.Challenges;
+using CSharp.Library.Tree;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class BinarySearchTreeClosestValueSearch : BaseTest
+    {
+        public BinarySearchTreeClosestValueSearch()
+        {
+            TypeToTest = typeof(FindClosestValueInBinarySearchTree);
+        }
+
+        private void TestImplementations(BinaryNode<int> node, int target, int expectedResult)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var invocationResult = (int?)implementation.Invoke(null, new object[] { node, target });
+                invocationResult.ShouldNotBe(null);
+                var actualResult = invocationResult.Value;
+                actualResult.ShouldBe(expectedResult);
+            }
+        }
+
+        [Fact]
+        public void ReturnsClosestValueTestCase01()
+        {
+            //           9
+            var binarySearchTree = BinaryTreeManager.Create(new int?[] { 9 });
+            const int target = 100;
+            const int expectedResult = 9;
+            TestImplementations(binarySearchTree.Root, target, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsClosestValueTestCase02()
+        {
+            //           9
+            //          / \
+            //         /   \
+            //        4     17
+            //       / \     \
+            //      3   6     22
+            //         / \   /
+            //        5   7 20
+            var binarySearchTree = BinaryTreeManager.Create(new int?[] { 9, 4, 17, 3, 6, null, 22, null, null, 5, 7, 20 });
+            const int target = 4;
+            const int expectedResult = 4;
+            TestImplementations(binarySearchTree.Root, target, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsClosestValueTestCase03()
+        {
+            //           9
+            //          / \
+            //         /   \
+            //        4     17
+            //       / \     \
+            //      3   6     22
+            //         / \   /
+            //        5   7 20
+            var binarySearchTree = BinaryTreeManager.Create(new int?[] { 9, 4, 17, 3, 6, null, 22, null, null, 5, 7, 20 });
+            const int target = 18;
+            const int expectedResult = 17;
+            TestImplementations(binarySearchTree.Root, target, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsClosestValueTestCase04()
+        {
+            //           9
+            //          / \
+            //         /   \
+            //        4     17
+            //       / \     \
+            //      3   6     22
+            //         / \   /
+            //        5   7 20
+            var binarySearchTree = BinaryTreeManager.Create(new int?[] { 9, 4, 17, 3, 6, null, 22, null, null, 5, 7, 20 });
+            const int target = 12;
+            const int expectedResult = 9;
+            TestImplementations(binarySearchTree.Root, target, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsClosestValueTestCase05()
+        {
+            //           10
+            //          /  \
+            //         /    \
+            //        5     15
+            //       / \   /  \
+            //      2   5 13  22
+            //     /        \
+            //    1         14
+            var binarySearchTree = BinaryTreeManager.Create(new int?[] { 10, 5, 15, 2, 5, 13, 22, 1, null, null, 14 });
+            const int target = 12;
+            const int expectedResult = 13;
+            TestImplementations(binarySearchTree.Root, target, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsClosestValueTestCase06()
+        {
+            //           10
+            //          /  \
+            //         /    \
+            //        5     15
+            //       / \   /  \
+            //      2   5 13  22
+            //     /        \
+            //    1         14
+            var binarySearchTree = BinaryTreeManager.Create(new int?[] { 10, 5, 15, 2, 5, 13, 22, 1, null, null, 14 });
+            const int target = 8;
+            const int expectedResult = 10;
+            TestImplementations(binarySearchTree.Root, target, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsClosestValueTestCase07()
+        {
+            //           10
+            //          /  \
+            //         /    \
+            //        5     15
+            //       / \   /  \
+            //      2   5 13  22
+            //     /        \
+            //    1         14
+            var binarySearchTree = BinaryTreeManager.Create(new int?[] { 10, 5, 15, 2, 5, 13, 22, 1, null, null, 14 });
+            const int target = 7;
+            const int expectedResult = 5;
+            TestImplementations(binarySearchTree.Root, target, expectedResult);
+        }
+
+        [Fact]
+        public void ReturnsClosestValueTestCase08()
+        {
+            //           10
+            //          /  \
+            //         /    \
+            //        3     15
+            //       / \   /  \
+            //      2   6 13  22
+            //     /        \
+            //    1         14
+            var binarySearchTree = BinaryTreeManager.Create(new int?[] { 10, 3, 15, 2, 6, 13, 22, 1, null, null, 14 });
+            const int target = 7;
+            const int expectedResult = 6;
+            TestImplementations(binarySearchTree.Root, target, expectedResult);
+        }
+    }
+}
