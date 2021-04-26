f301d92be5735eb832131eecfbf8d9993e415ab9
Lesair Valmont <ixra.net@gmail.com>
2021-04-26 17:38:21 -0500

    FindMaxValueDoublingOnMatch challenge.

---
+++ src/CSharp/Challenges/FindMaxValueDoublingOnMatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Source: GeeksforGeeks
+    ///     https://www.geeksforgeeks.org/find-final-value-if-we-double-after-every-successful-search-in-array/
+    /// </summary>
+    public static class FindMaxValueDoublingOnMatch
+    {
+        /// <summary>
+        ///     Iterative. Binary search.
+        ///     Time complexity: O(n log n).
+        ///     Space complexity: O(n).
+        /// </summary>
+        public static int BinarySearchImplementation(int[] numbers, int k)
+        {
+            Array.Sort(numbers);
+            while (Array.BinarySearch(numbers, k) >= 0)
+                k *= 2;
+
+            return k;
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/DoublingOnMatchMaxValueSearch.cs
@@ -0,0 +1,70 @@
+﻿using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class DoublingOnMatchMaxValueSearch : BaseTest
+    {
+        public DoublingOnMatchMaxValueSearch()
+        {
+            TypeToTest = typeof(FindMaxValueDoublingOnMatch);
+        }
+
+        private void TestImplementations(int[] numbers, int k, int expected)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var invocationResult = (int?) implementation.Invoke(null, new object[] {numbers, k});
+                invocationResult.ShouldNotBeNull();
+                var actual = invocationResult.Value;
+                actual.ShouldBe(expected);
+            }
+        }
+
+        [Fact]
+        public void ReturnsTheMaximumValueTestCase01()
+        {
+            var numbers = new[] {2, 3, 4, 10, 8, 1};
+            const int k = 2;
+            var expected = 16;
+            TestImplementations(numbers, k, expected);
+        }
+
+        [Fact]
+        public void ReturnsTheMaximumValueTestCase02()
+        {
+            var numbers = new[] {4};
+            const int k = 2;
+            var expected = 2;
+            TestImplementations(numbers, k, expected);
+        }
+
+        [Fact]
+        public void ReturnsTheMaximumValueTestCase03()
+        {
+            var numbers = new[] {4};
+            const int k = 4;
+            var expected = 8;
+            TestImplementations(numbers, k, expected);
+        }
+
+        [Fact]
+        public void ReturnsTheMaximumValueTestCase04()
+        {
+            var numbers = new[] {8, 8, 0, 4, 3, 2, -2};
+            const int k = 2;
+            var expected = 16;
+            TestImplementations(numbers, k, expected);
+        }
+
+        [Fact]
+        public void ReturnsTheMaximumValueTestCase05()
+        {
+            var numbers = new[] {2, 4, 5, 6, 7};
+            const int k = 3;
+            var expected = 3;
+            TestImplementations(numbers, k, expected);
+        }
+    }
+}
