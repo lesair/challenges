88c60d93000c4f908946fcfe5a1aad53706106e3
Lesair Valmont <ixra.net@gmail.com>
2021-07-09 15:58:04 -0500

    Bear and Big Brother challenge.

---
+++ src/CSharp/Challenges/BearAndBigBrother.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Bear and Big Brother
+    ///     Source: Codeforces.
+    ///     https://codeforces.com/problemset/problem/791/A
+    /// </summary>
+    public static class BearAndBigBrother
+    {
+        public static int GetsValue(int limak, int bob)
+        {
+            var years = 0;
+            while (limak <= bob)
+            {
+                limak *= 3;
+                bob *= 2;
+                years++;
+            }
+
+            return years;
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/BearAndBigBrother.cs
@@ -0,0 +1,39 @@
+﻿using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class BearAndBigBrotherTest
+    {
+        [Fact]
+        public void ReturnsExpectedValueTestCase01()
+        {
+            const int limak = 4;
+            const int bob = 7;
+            const int expected = 2;
+            var actual = BearAndBigBrother.GetsValue(limak, bob);
+            actual.ShouldBe(expected);
+        }
+
+        [Fact]
+        public void ReturnsExpectedValueTestCase02()
+        {
+            const int limak = 4;
+            const int bob = 9;
+            const int expected = 3;
+            var actual = BearAndBigBrother.GetsValue(limak, bob);
+            actual.ShouldBe(expected);
+        }
+
+        [Fact]
+        public void ReturnsExpectedValueTestCase03()
+        {
+            const int limak = 1;
+            const int bob = 1;
+            const int expected = 1;
+            var actual = BearAndBigBrother.GetsValue(limak, bob);
+            actual.ShouldBe(expected);
+        }
+    }
+}
