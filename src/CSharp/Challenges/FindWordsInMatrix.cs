8ed63ae447004a810e3c7121729a5fe11eca87df
Lesair Valmont <ixra.net@gmail.com>
2021-11-24 17:42:00 -0600

    FindWordsInMatrix challenge.

---
+++ src/CSharp/Challenges/FindWordsInMatrix.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Given an m x n board of characters and a list of strings words, return all words on the board.
+    ///     Backtracking, tries.
+    ///     Source: LeetCode.
+    ///     https://leetcode.com/problems/word-search-ii/
+    /// </summary>
+    public static class FindWordsInMatrix
+    {
+        public static TrieNode BadPrefixes { get; set; } = new(string.Empty);
+
+        public static IEnumerable<string> Implementation(char[][] board, string[] words)
+        {
+            var wordsFound = new HashSet<string>();
+
+            foreach (var word in words)
+            {
+                var wordInBadPrefixes = false;
+                var wordFound = false;
+                for (var i = 0; i < word.Length - 0; i++)
+                {
+                    if (!BadPrefixes.ContainsPrefix(word[..^i])) continue;
+                    wordInBadPrefixes = true;
+                    break;
+                }
+
+                if (wordInBadPrefixes)
+                    continue;
+
+                var maxMatchIndex = -1;
+                for (var y = 0; y < board.Length; y++)
+                {
+                    if (wordFound)
+                        break;
+                    for (var x = 0; x < board[0].Length; x++)
+                    {
+                        if (wordFound)
+                            break;
+
+                        int matchIndex;
+                        (wordFound, matchIndex) = FindWordInBoard(word, board, y, x);
+                        if (wordFound)
+                            wordsFound.Add(word);
+                        maxMatchIndex = Math.Max(maxMatchIndex, matchIndex);
+                    }
+                }
+
+                if (!wordFound)
+                    BadPrefixes.AddPrefix(word[..(maxMatchIndex + 2)]);
+            }
+
+            return wordsFound;
+        }
+
+        private static (bool IsFound, int matchIndex) FindWordInBoard(string word, IReadOnlyList<char[]> board, int y,
+            int x, int matchIndex = 0)
+        {
+            if (matchIndex == word.Length)
+                return (true, --matchIndex);
+
+            if (x < 0 || y < 0 || y == board.Count || x == board[0].Length)
+                return (false, --matchIndex);
+
+            if (board[y][x] == '*')
+                return (false, --matchIndex);
+
+            if (word[matchIndex] != board[y][x])
+                return (false, --matchIndex);
+
+            var newBoard = board.Select(a => a.ToArray()).ToArray();
+            newBoard[y][x] = '*';
+
+            var maxMatchIndex = matchIndex;
+            var directions = new[] { (0, 1), (1, 0), (0, -1), (-1, 0) };
+            foreach (var (dirY, dirX) in directions)
+            {
+                var (found, foundMatchIndex) = FindWordInBoard(word, newBoard, y + dirY, x + dirX, matchIndex + 1);
+                if (found)
+                    return (true, foundMatchIndex);
+                maxMatchIndex = Math.Max(maxMatchIndex, foundMatchIndex);
+            }
+
+            return (false, maxMatchIndex);
+        }
+    }
+
+    public class TrieNode : IEquatable<TrieNode>
+    {
+        public TrieNode(string data)
+        {
+            Data = data;
+        }
+
+        public string Data { get; }
+        public HashSet<TrieNode> Children { get; set; } = new();
+
+        public bool Equals(TrieNode other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Data == other.Data;
+        }
+
+        public void AddPrefix(string s, int prefixIndex = 0)
+        {
+            var targetChildNode = new TrieNode(s[..++prefixIndex]);
+
+            if (!Children.Contains(targetChildNode))
+                Children.Add(targetChildNode);
+
+            if (prefixIndex == s.Length)
+                return;
+
+            Children.TryGetValue(targetChildNode, out var foundChildNode);
+            foundChildNode?.AddPrefix(s, prefixIndex);
+        }
+
+        public bool ContainsPrefix(string s, int prefixIndex = 0)
+        {
+            if (prefixIndex == s.Length)
+                return Data == s && !Children.Any();
+
+            var targetChildNode = new TrieNode(s[..++prefixIndex]);
+
+            if (!Children.Contains(targetChildNode))
+                return false;
+
+            Children.TryGetValue(targetChildNode, out var foundChildNode);
+            return foundChildNode?.ContainsPrefix(s, prefixIndex) ?? false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((TrieNode)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Data != null ? Data.GetHashCode() : 0;
+        }
+
+        public static bool operator ==(TrieNode left, TrieNode right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(TrieNode left, TrieNode right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override string ToString()
+        {
+            return $"{Data} [{string.Join(", ", Children)}]";
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/WordsInMatrixSearch.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using CSharp.Challenges;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class WordsInMatrixSearch : BaseTest
+    {
+        public WordsInMatrixSearch()
+        {
+            TypeToTest = typeof(FindWordsInMatrix);
+        }
+
+        private void TestImplementations(IEnumerable board, IEnumerable words, ICollection<string> expectedResults)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var actualResults = ((IEnumerable<string>)implementation.Invoke(null, new object[] { board, words }))
+                    ?.ToList();
+                actualResults.ShouldNotBeNull();
+                actualResults.ShouldBe(expectedResults, true);
+            }
+        }
+
+        [Fact]
+        public void ReturnsExpectedWordsTestCase01()
+        {
+            var board = new[]
+            {
+                new[] { 'a', 'e' },
+                new[] { 'o', 'i' }
+            };
+            var words = new[] { "xe", "x", "z", "ze", "ae", "aea", "aeaoi", "aei", "aeio", "aeioa", "ioae" };
+            var expectedResults = new[] { "ae", "aei", "aeio", "ioae" };
+            TestImplementations(board, words, expectedResults);
+        }
+
+        [Fact]
+        public void ReturnsExpectedWordsTestCase02()
+        {
+            var board = new[]
+            {
+                new[] { 'o', 'a', 'a', 'n' },
+                new[] { 'e', 't', 'a', 'e' },
+                new[] { 'i', 'h', 'k', 'r' },
+                new[] { 'i', 'f', 'l', 'v' }
+            };
+            var words = new[] { "oath", "pea", "eat", "rain" };
+            var expectedResults = new[] { "eat", "oath" };
+            TestImplementations(board, words, expectedResults);
+        }
+
+        [Fact]
+        public void ReturnsExpectedWordsTestCase03()
+        {
+            var board = new[]
+            {
+                new[] { 'a', 'b', 'a' },
+                new[] { 'x', 'y', 'c' },
+                new[] { 'z', 's', 'u' }
+            };
+            var words = new[] { "abac", "abacus" };
+            var expectedResults = new[] { "abac", "abacus" };
+            TestImplementations(board, words, expectedResults);
+        }
+
+        [Fact]
+        public void ReturnsExpectedWordsLargeTestCase01()
+        {
+            var board = new[]
+            {
+                new[] { 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a' },
+                new[] { 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b' },
+                new[] { 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a' },
+                new[] { 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b' },
+                new[] { 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a' },
+                new[] { 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b' },
+                new[] { 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a' },
+                new[] { 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b' },
+                new[] { 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a' },
+                new[] { 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b' }
+            };
+            var words = new[]
+            {
+                "ababababaa", "ababababab", "ababababac", "ababababad", "ababababae", "ababababaf", "ababababag",
+                "ababababah", "ababababai", "ababababaj", "ababababak", "ababababal", "ababababam", "ababababan",
+                "ababababao", "ababababap", "ababababaq", "ababababar", "ababababas", "ababababat", "ababababau",
+                "ababababav", "ababababaw", "ababababax", "ababababay", "ababababaz", "ababababba", "ababababbb",
+                "ababababbc", "ababababbd", "ababababbe", "ababababbf", "ababababbg", "ababababbh", "ababababbi",
+                "ababababbj", "ababababbk", "ababababbl", "ababababbm", "ababababbn", "ababababbo", "ababababbp",
+                "ababababbq", "ababababbr", "ababababbs", "ababababbt", "ababababbu", "ababababbv", "ababababbw",
+                "ababababbx", "ababababby", "ababababbz", "ababababca", "ababababcb", "ababababcc", "ababababcd",
+                "ababababce", "ababababcf", "ababababcg", "ababababch", "ababababci", "ababababcj", "ababababck",
+                "ababababcl", "ababababcm", "ababababcn", "ababababco", "ababababcp", "ababababcq", "ababababcr",
+                "ababababcs", "ababababct", "ababababcu", "ababababcv", "ababababcw", "ababababcx", "ababababcy",
+                "ababababcz", "ababababda", "ababababdb", "ababababdc", "ababababdd", "ababababde", "ababababdf",
+                "ababababdg", "ababababdh", "ababababdi", "ababababdj", "ababababdk", "ababababdl", "ababababdm",
+                "ababababdn", "ababababdo", "ababababdp", "ababababdq", "ababababdr", "ababababds", "ababababdt",
+                "ababababdu", "ababababdv", "ababababdw", "ababababdx", "ababababdy", "ababababdz", "ababababea",
+                "ababababeb", "ababababec", "ababababed", "ababababee", "ababababef", "ababababeg", "ababababeh",
+                "ababababei", "ababababej", "ababababek", "ababababel", "ababababem", "ababababen", "ababababeo",
+                "ababababep", "ababababeq", "ababababer", "ababababes", "ababababet", "ababababeu", "ababababev",
+                "ababababew", "ababababex", "ababababey", "ababababez", "ababababfa", "ababababfb", "ababababfc",
+                "ababababfd", "ababababfe", "ababababff", "ababababfg", "ababababfh", "ababababfi", "ababababfj",
+                "ababababfk", "ababababfl", "ababababfm", "ababababfn", "ababababfo", "ababababfp", "ababababfq",
+                "ababababfr", "ababababfs", "ababababft", "ababababfu", "ababababfv", "ababababfw", "ababababfx",
+                "ababababfy", "ababababfz", "ababababga", "ababababgb", "ababababgc", "ababababgd", "ababababge",
+                "ababababgf", "ababababgg", "ababababgh", "ababababgi", "ababababgj", "ababababgk", "ababababgl",
+                "ababababgm", "ababababgn", "ababababgo", "ababababgp", "ababababgq", "ababababgr", "ababababgs",
+                "ababababgt", "ababababgu", "ababababgv", "ababababgw", "ababababgx", "ababababgy", "ababababgz",
+                "ababababha", "ababababhb", "ababababhc", "ababababhd", "ababababhe", "ababababhf", "ababababhg",
+                "ababababhh", "ababababhi", "ababababhj", "ababababhk", "ababababhl", "ababababhm", "ababababhn",
+                "ababababho", "ababababhp", "ababababhq", "ababababhr", "ababababhs", "ababababht", "ababababhu",
+                "ababababhv", "ababababhw", "ababababhx", "ababababhy", "ababababhz", "ababababia", "ababababib",
+                "ababababic", "ababababid", "ababababie", "ababababif", "ababababig", "ababababih", "ababababii",
+                "ababababij", "ababababik", "ababababil", "ababababim", "ababababin", "ababababio", "ababababip",
+                "ababababiq", "ababababir", "ababababis", "ababababit", "ababababiu", "ababababiv", "ababababiw",
+                "ababababix", "ababababiy", "ababababiz", "ababababja", "ababababjb", "ababababjc", "ababababjd",
+                "ababababje", "ababababjf", "ababababjg", "ababababjh", "ababababji", "ababababjj", "ababababjk",
+                "ababababjl", "ababababjm", "ababababjn", "ababababjo", "ababababjp", "ababababjq", "ababababjr",
+                "ababababjs", "ababababjt", "ababababju", "ababababjv", "ababababjw", "ababababjx", "ababababjy",
+                "ababababjz", "ababababka", "ababababkb", "ababababkc", "ababababkd", "ababababke", "ababababkf",
+                "ababababkg", "ababababkh", "ababababki", "ababababkj", "ababababkk", "ababababkl", "ababababkm",
+                "ababababkn", "ababababko", "ababababkp", "ababababkq", "ababababkr", "ababababks", "ababababkt",
+                "ababababku", "ababababkv", "ababababkw", "ababababkx", "ababababky", "ababababkz", "ababababla",
+                "abababablb", "abababablc", "ababababld", "abababable", "abababablf", "abababablg", "abababablh",
+                "ababababli", "abababablj", "abababablk", "ababababll", "abababablm", "ababababln", "abababablo",
+                "abababablp", "abababablq", "abababablr", "ababababls", "abababablt", "abababablu", "abababablv",
+                "abababablw", "abababablx", "abababably", "abababablz", "ababababma", "ababababmb", "ababababmc",
+                "ababababmd", "ababababme", "ababababmf", "ababababmg", "ababababmh", "ababababmi", "ababababmj",
+                "ababababmk", "ababababml", "ababababmm", "ababababmn", "ababababmo", "ababababmp", "ababababmq",
+                "ababababmr", "ababababms", "ababababmt", "ababababmu", "ababababmv", "ababababmw", "ababababmx",
+                "ababababmy", "ababababmz", "ababababna", "ababababnb", "ababababnc", "ababababnd", "ababababne",
+                "ababababnf", "ababababng", "ababababnh", "ababababni", "ababababnj", "ababababnk", "ababababnl",
+                "ababababnm", "ababababnn", "ababababno", "ababababnp", "ababababnq", "ababababnr", "ababababns",
+                "ababababnt", "ababababnu", "ababababnv", "ababababnw", "ababababnx", "ababababny", "ababababnz",
+                "ababababoa", "ababababob", "ababababoc", "ababababod", "ababababoe", "ababababof", "ababababog",
+                "ababababoh", "ababababoi", "ababababoj", "ababababok", "ababababol", "ababababom", "ababababon",
+                "ababababoo", "ababababop", "ababababoq", "ababababor", "ababababos", "ababababot", "ababababou",
+                "ababababov", "ababababow", "ababababox", "ababababoy", "ababababoz", "ababababpa", "ababababpb",
+                "ababababpc", "ababababpd", "ababababpe", "ababababpf", "ababababpg", "ababababph", "ababababpi",
+                "ababababpj", "ababababpk", "ababababpl", "ababababpm", "ababababpn", "ababababpo", "ababababpp",
+                "ababababpq", "ababababpr", "ababababps", "ababababpt", "ababababpu", "ababababpv", "ababababpw",
+                "ababababpx", "ababababpy", "ababababpz", "ababababqa", "ababababqb", "ababababqc", "ababababqd",
+                "ababababqe", "ababababqf", "ababababqg", "ababababqh", "ababababqi", "ababababqj", "ababababqk",
+                "ababababql", "ababababqm", "ababababqn", "ababababqo", "ababababqp", "ababababqq", "ababababqr",
+                "ababababqs", "ababababqt", "ababababqu", "ababababqv", "ababababqw", "ababababqx", "ababababqy",
+                "ababababqz", "ababababra", "ababababrb", "ababababrc", "ababababrd", "ababababre", "ababababrf",
+                "ababababrg", "ababababrh", "ababababri", "ababababrj", "ababababrk", "ababababrl", "ababababrm",
+                "ababababrn", "ababababro", "ababababrp", "ababababrq", "ababababrr", "ababababrs", "ababababrt",
+                "ababababru", "ababababrv", "ababababrw", "ababababrx", "ababababry", "ababababrz", "ababababsa",
+                "ababababsb", "ababababsc", "ababababsd", "ababababse", "ababababsf", "ababababsg", "ababababsh",
+                "ababababsi", "ababababsj", "ababababsk", "ababababsl", "ababababsm", "ababababsn", "ababababso",
+                "ababababsp", "ababababsq", "ababababsr", "ababababss", "ababababst", "ababababsu", "ababababsv",
+                "ababababsw", "ababababsx", "ababababsy", "ababababsz", "ababababta", "ababababtb", "ababababtc",
+                "ababababtd", "ababababte", "ababababtf", "ababababtg", "ababababth", "ababababti", "ababababtj",
+                "ababababtk", "ababababtl", "ababababtm", "ababababtn", "ababababto", "ababababtp", "ababababtq",
+                "ababababtr", "ababababts", "ababababtt", "ababababtu", "ababababtv", "ababababtw", "ababababtx",
+                "ababababty", "ababababtz", "ababababua", "ababababub", "ababababuc", "ababababud", "ababababue",
+                "ababababuf", "ababababug", "ababababuh", "ababababui", "ababababuj", "ababababuk", "ababababul",
+                "ababababum", "ababababun", "ababababuo", "ababababup", "ababababuq", "ababababur", "ababababus",
+                "ababababut", "ababababuu", "ababababuv", "ababababuw", "ababababux", "ababababuy", "ababababuz",
+                "ababababva", "ababababvb", "ababababvc", "ababababvd", "ababababve", "ababababvf", "ababababvg",
+                "ababababvh", "ababababvi", "ababababvj", "ababababvk", "ababababvl", "ababababvm", "ababababvn",
+                "ababababvo", "ababababvp", "ababababvq", "ababababvr", "ababababvs", "ababababvt", "ababababvu",
+                "ababababvv", "ababababvw", "ababababvx", "ababababvy", "ababababvz", "ababababwa", "ababababwb",
+                "ababababwc", "ababababwd", "ababababwe", "ababababwf", "ababababwg", "ababababwh", "ababababwi",
+                "ababababwj", "ababababwk", "ababababwl", "ababababwm", "ababababwn", "ababababwo", "ababababwp",
+                "ababababwq", "ababababwr", "ababababws", "ababababwt", "ababababwu", "ababababwv", "ababababww",
+                "ababababwx", "ababababwy", "ababababwz", "ababababxa", "ababababxb", "ababababxc", "ababababxd",
+                "ababababxe", "ababababxf", "ababababxg", "ababababxh", "ababababxi", "ababababxj", "ababababxk",
+                "ababababxl", "ababababxm", "ababababxn", "ababababxo", "ababababxp", "ababababxq", "ababababxr",
+                "ababababxs", "ababababxt", "ababababxu", "ababababxv", "ababababxw", "ababababxx", "ababababxy",
+                "ababababxz", "ababababya", "ababababyb", "ababababyc", "ababababyd", "ababababye", "ababababyf",
+                "ababababyg", "ababababyh", "ababababyi", "ababababyj", "ababababyk", "ababababyl", "ababababym",
+                "ababababyn", "ababababyo", "ababababyp", "ababababyq", "ababababyr", "ababababys", "ababababyt",
+                "ababababyu", "ababababyv", "ababababyw", "ababababyx", "ababababyy", "ababababyz", "ababababza",
+                "ababababzb", "ababababzc", "ababababzd", "ababababze", "ababababzf", "ababababzg", "ababababzh",
+                "ababababzi", "ababababzj", "ababababzk", "ababababzl", "ababababzm", "ababababzn", "ababababzo",
+                "ababababzp", "ababababzq", "ababababzr", "ababababzs", "ababababzt", "ababababzu", "ababababzv",
+                "ababababzw", "ababababzx", "ababababzy", "ababababzz"
+            };
+            var expectedResults = new[] { "ababababab" };
+            TestImplementations(board, words, expectedResults);
+        }
+    }
+}
