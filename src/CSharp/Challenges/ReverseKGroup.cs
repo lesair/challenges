9ea6d4d5833974fd4ee49d75d8f6c9491eeabab9
Lesair Valmont <ixra.net@gmail.com>
2022-01-03 09:38:58 -0600

    Reverse K Group challenge.

---
+++ src/CSharp/Challenges/ReverseKGroup.cs
@@ -0,0 +1,67 @@
+﻿using CSharp.Library.SinglyLinkedList;
+
+namespace CSharp.Challenges
+{
+    /// <summary>
+    ///     Reverses nodes in K-Group
+    ///     Source: LeetCode
+    ///     https://leetcode.com/problems/reverse-nodes-in-k-group/
+    /// </summary>
+    public static class ReverseKGroup
+    {
+        /// <summary>
+        ///     Recursive. Easy to follow.
+        ///     Multiple pointers manipulation.
+        ///     Time complexity: O(n).
+        ///     Space complexity: O(1).
+        /// </summary>
+        public static Node<int> RecursiveImplementation(Node<int> head, int k)
+        {
+            Node<int> JumpNodes(Node<int> node, int jumps)
+            {
+                var counter = 1;
+                do
+                {
+                    node = node?.Next;
+                } while (++counter <= jumps && node != null);
+
+                return node;
+            }
+
+
+            Node<int> Reverse(Node<int> previousNode, Node<int> currentNode, int nodesCounter = 1)
+            {
+                if (currentNode == null)
+                    return null;
+
+                var nextNode = currentNode.Next;
+                currentNode.Next = previousNode;
+
+                return nextNode != null && nodesCounter < k
+                    ? Reverse(currentNode, nextNode, nodesCounter + 1)
+                    : currentNode;
+            }
+
+            Node<int> newMainHead = null;
+            Node<int> previousTail = null;
+
+            var currentHead = head;
+
+            while (JumpNodes(currentHead, k - 1) != null)
+            {
+                var nextHead = JumpNodes(currentHead, k);
+
+                var newTail = currentHead;
+                var newHead = Reverse(null, currentHead);
+                if (previousTail != null)
+                    previousTail.Next = newHead;
+                newTail.Next = nextHead;
+                newMainHead ??= newHead;
+                previousTail = newTail;
+                currentHead = JumpNodes(newHead, k);
+            }
+
+            return newMainHead ?? currentHead;
+        }
+    }
+}
---
+++ tests/CSharp-unit-tests/Challenges/KGroupReversal.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using CSharp.Challenges;
+using CSharp.Library.SinglyLinkedList;
+using Shouldly;
+using Xunit;
+
+namespace CSharp
+{
+    public class KGroupReversal : BaseTest
+    {
+        public KGroupReversal()
+        {
+            TypeToTest = typeof(ReverseKGroup);
+        }
+
+        private void TestImplementations(int k, IReadOnlyCollection<int> linkedListData,
+            IReadOnlyCollection<int> expectedReversedListData)
+        {
+            foreach (var implementation in ImplementationsToTest())
+            {
+                var headNode = new SinglyLinkedListManager<int>().Create(linkedListData);
+                var reversedListHeadNode = (Node<int>)implementation.Invoke(null, new object[] { headNode, k });
+                var actualReversedListData = SinglyLinkedListManager<int>.GetData(reversedListHeadNode);
+                actualReversedListData.ShouldBe(expectedReversedListData);
+            }
+        }
+
+        [Fact]
+        public void DoesNotReverseBecauseKLargerThanList()
+        {
+            const int k = 5;
+            var linkedListData = new[] { 1, 2, 3 };
+            var expectedReversedListData = new[] { 1, 2, 3 };
+            TestImplementations(k, linkedListData, expectedReversedListData);
+        }
+
+        [Fact]
+        public void DoesNotReverseBecauseKIsOne()
+        {
+            const int k = 1;
+            var linkedListData = new[] { 1, 2, 3, 4, 5 };
+            var expectedReversedListData = new[] { 1, 2, 3, 4, 5 };
+            TestImplementations(k, linkedListData, expectedReversedListData);
+        }
+
+        [Fact]
+        public void DoesNotReverseBecauseListHasOneNode()
+        {
+            const int k = 1;
+            var linkedListData = new[] { 5 };
+            var expectedReversedListData = new[] { 5 };
+            TestImplementations(k, linkedListData, expectedReversedListData);
+        }
+
+        [Fact]
+        public void CorrectlyReversesALinkedListForKEqualsToListLength()
+        {
+            const int k = 5;
+            var linkedListData = new[] { 1, 2, 3, 4, 5 };
+            var expectedReversedListData = new[] { 5, 4, 3, 2, 1 };
+            TestImplementations(k, linkedListData, expectedReversedListData);
+        }
+
+        [Fact]
+        public void CorrectlyReversesALinkedListKFitsOnce()
+        {
+            const int k = 3;
+            var linkedListData = new[] { 1, 2, 3, 4, 5 };
+            var expectedReversedListData = new[] { 3, 2, 1, 4, 5 };
+            TestImplementations(k, linkedListData, expectedReversedListData);
+        }
+
+        [Fact]
+        public void CorrectlyReversesALinkedListKFitsTwice()
+        {
+            const int k = 2;
+            var linkedListData = new[] { 1, 2, 3, 4, 5 };
+            var expectedReversedListData = new[] { 2, 1, 4, 3, 5 };
+            TestImplementations(k, linkedListData, expectedReversedListData);
+        }
+
+        [Fact]
+        public void CorrectlyReversesALinkedListManyTimes()
+        {
+            const int k = 3;
+            var linkedListData = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            var expectedReversedListData = new[] { 3, 2, 1, 6, 5, 4, 9, 8, 7, 12, 11, 10 };
+            TestImplementations(k, linkedListData, expectedReversedListData);
+        }
+    }
+}
